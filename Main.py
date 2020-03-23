2a2f2080fd52b7ce2603d348e9884c60daa95e62
iquitgame <admm64@gmail.com>
2020-03-22 22:09:45 -0400

    Custom neural network and neural network from pytorch

--- Main.py
+++ Main.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
-
-
-
+import torch
+import torchvision
 
 if __name__ == '__main__':
     print("Hello, World")
@@ -17,6 +16,10 @@ if __name__ == '__main__':
     cap.set(4, 720) #set Height = 300
     #cap.set(12, 0.1)
 
+    torchvision.model
+    resnext = torchvision.models.resnext101_32x8d(pretrained=True,progress=True)
+
+
     sign = 'DEFAULT'
 
     while(True):
@@ -24,7 +27,7 @@ if __name__ == '__main__':
         #Gets rid of the info on the bottom bar
         #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
         #maskedFrame = subtractor.apply(frame)
-        
+
         frame = cv2.putText(frame, sign, (10,30), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
         cv2.imshow('frame',frame)
         if(cv2.waitKey(1) & 0xFF == ord(' ')): #27= esc key but I don't know where the constant is being kept  #ord(' ')):
---
+++ aslnn.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3,6,5)
